4eed120d64a07972a1a05330149701bdac8cdec7
Sruthiravichnadran <126389806+Sruthiravichnadran@users.noreply.github.com>
2023-06-29 13:16:31 -0400

    JavaScript Code

---
+++ script.js
@@ -0,0 +1,36 @@
+// Get necessary elements
+const titleInput = document.getElementById("title-input");
+const entryInput = document.getElementById("entry-input");
+const addBtn = document.getElementById("add-btn");
+const entryList = document.getElementById("entry-list");
+
+// Function to add an entry
+function addEntry() {
+  const titleText = titleInput.value.trim();
+  const entryText = entryInput.value.trim();
+  const currentDate = new Date().toDateString();
+  
+  if (titleText !== "" && entryText !== "") {
+    const li = document.createElement("li");
+    const title = document.createElement("p");
+    const entry = document.createElement("p");
+    const date = document.createElement("p");
+    
+    title.className = "entry-title";
+    title.textContent = titleText;
+    entry.textContent = entryText;
+    date.textContent = currentDate;
+    
+    li.appendChild(title);
+    li.appendChild(entry);
+    li.appendChild(date);
+    
+    entryList.appendChild(li);
+    
+    titleInput.value = "";
+    entryInput.value = "";
+  }
+}
+
+// Event listener for the add button
+addBtn.addEventListener("click", addEntry);
